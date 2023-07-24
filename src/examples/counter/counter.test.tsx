146c25936dba78433037b0eed0a01c367dd5b821
Ajaypal Cheema <ajaypalster@gmail.com>
2023-07-23 20:22:24 -0400

    component test

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -2,9 +2,15 @@ import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('0');
+  const incrementBtn = screen.getByRole('button', { name: 'Increment' });
+  await userEvent.click(incrementBtn);
+  expect(currentCount).toHaveTextContent('1');
+});
